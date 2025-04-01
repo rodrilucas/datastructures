3b2e96acbfa9ec844fbfe3db1fa81fe2d9311009
rodrilucas <lucas.celery@gmail.com>
2025-04-01 15:05:26 -0300

    linked list

---
+++ LinkedList/src/LinkedList.java
@@ -0,0 +1,32 @@
+public class LinkedList {
+    private Node head;
+    private Node tail;
+    private int length;
+
+    public LinkedList(int value){
+        Node newNode = new Node(value);
+        head = newNode;
+        tail = newNode;
+        length = 1;
+    }
+
+    public void printList(){
+        Node temp = head;
+        while (temp != null){
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+    }
+
+    public int getHead() {
+        return head.value;
+    }
+
+    public int getTail() {
+        return tail.value;
+    }
+
+    public int getLength() {
+        return length;
+    }
+}
