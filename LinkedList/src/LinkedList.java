7228c134b85a19cbaa044e7bda85250c3939f846
rodrilucas <lucas.celery@gmail.com>
2025-04-01 15:13:15 -0300

    ll append

--- LinkedList/src/LinkedList.java
+++ LinkedList/src/LinkedList.java
@@ -3,16 +3,28 @@ public class LinkedList {
     private Node tail;
     private int length;
 
-    public LinkedList(int value){
+    public LinkedList(int value) {
         Node newNode = new Node(value);
         head = newNode;
         tail = newNode;
         length = 1;
     }
 
-    public void printList(){
+    public void append(int value) {
+        Node newNode = new Node(value);
+        if (length == 0){
+            head = newNode;
+            tail = newNode;
+        }else {
+            tail.next = newNode;
+            tail = newNode;
+        }
+        length++;
+    }
+
+    public void printList() {
         Node temp = head;
-        while (temp != null){
+        while (temp != null) {
             System.out.println(temp.value);
             temp = temp.next;
         }
