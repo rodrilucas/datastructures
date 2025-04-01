4bc818be0c75d265fa25525cfbbf6f755c96976a
rodrilucas <lucas.celery@gmail.com>
2025-04-01 15:54:29 -0300

    ll remove

--- LinkedList/src/LinkedList.java
+++ LinkedList/src/LinkedList.java
@@ -12,91 +12,91 @@ public class LinkedList {
 
     public void append(int value) {
         Node newNode = new Node(value);
-        if (length == 0){
+        if (length == 0) {
             head = newNode;
             tail = newNode;
-        }else {
+        } else {
             tail.next = newNode;
             tail = newNode;
         }
         length++;
     }
 
-    public Node removeLast(){
-        if (length == 0){
+    public Node removeLast() {
+        if (length == 0) {
             return null;
         }
         Node temp = head;
         Node prev = temp;
-        while (temp.next != null){
+        while (temp.next != null) {
             prev = temp;
             temp = temp.next;
         }
         tail = prev;
         tail.next = null;
         length--;
-        if (length == 0){
+        if (length == 0) {
             head = null;
             temp = null;
         }
         return temp;
     }
 
-    public void prepend(int value){
+    public void prepend(int value) {
         Node newNode = new Node(value);
-        if (length == 0){
+        if (length == 0) {
             head = newNode;
             tail = newNode;
-        }else {
+        } else {
             newNode.next = head;
             head = newNode;
         }
         length++;
     }
 
-    public Node removeFirst(){
-        if (length == 0){
+    public Node removeFirst() {
+        if (length == 0) {
             return null;
         }
         Node temp = head;
         head = head.next;
         temp.next = null;
         length--;
-        if (length == 0){
+        if (length == 0) {
             tail = null;
         }
         return temp;
     }
 
-    public Node get(int index){
-        if (index < 0 || index >= length){
+    public Node get(int index) {
+        if (index < 0 || index >= length) {
             return null;
         }
         Node temp = head;
-        for (int i = 0; i < index; i++){
+        for (int i = 0; i < index; i++) {
             temp = temp.next;
         }
         return temp;
     }
 
-   public boolean set(int index, int value){
+    public boolean set(int index, int value) {
         Node temp = get(index);
-        if (temp != null){
+        if (temp != null) {
             temp.value = value;
             return true;
         }
         return false;
-   }
+    }
 
-   public boolean insert(int index, int value){
-        if (index < 0 || index > length){
+    public boolean insert(int index, int value) {
+        if (index < 0 || index > length) {
             return false;
         }
-        if (index == 0){
+        if (index == 0) {
             prepend(value);
             return true;
         }
-        if (index == length){
+        if (index == length) {
             append(value);
             return true;
         }
@@ -106,7 +106,25 @@ public class LinkedList {
         temp.next = newNode;
         length++;
         return true;
-   }
+    }
+
+    public Node remove(int index) {
+        if (index < 0 || index >= length) {
+            return null;
+        }
+        if (index == length - 1) {
+            return removeLast();
+        }
+        if (index == 0) {
+            return removeFirst();
+        }
+        Node prev = get(index - 1);
+        Node temp = prev.next;
+        prev.next = temp.next;
+        temp.next = null;
+        length--;
+        return temp;
+    }
 
     public void printList() {
         Node temp = head;
