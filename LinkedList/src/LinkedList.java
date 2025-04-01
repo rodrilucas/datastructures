ef707052dd885a63b4469f6453f8784153057e9e
rodrilucas <lucas.celery@gmail.com>
2025-04-01 15:33:27 -0300

    ll set

--- LinkedList/src/LinkedList.java
+++ LinkedList/src/LinkedList.java
@@ -79,6 +79,15 @@ public class LinkedList {
         return temp;
     }
 
+   public boolean set(int index, int value){
+        Node temp = get(index);
+        if (temp != null){
+            temp.value = value;
+            return true;
+        }
+        return false;
+   }
+
     public void printList() {
         Node temp = head;
         while (temp != null) {
