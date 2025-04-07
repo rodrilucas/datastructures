47356c8bef81bfd703c55d32e38aad137b6a6388
rodrilucas <lucas.celery@gmail.com>
2025-04-07 14:20:02 -0300

    dll remove last

--- DoublyLinkedList/src/DoublyLinkedList.java
+++ DoublyLinkedList/src/DoublyLinkedList.java
@@ -3,19 +3,19 @@ public class DoublyLinkedList {
     private Node tail;
     private int length;
 
-    public DoublyLinkedList(int value){
+    public DoublyLinkedList(int value) {
         Node newNode = new Node(value);
         head = newNode;
         tail = newNode;
         length = 1;
     }
 
-    public void append(int value){
+    public void append(int value) {
         Node newNode = new Node(value);
-        if (this.getLength() == 0){
+        if (this.getLength() == 0) {
             head = newNode;
             tail = newNode;
-        }else {
+        } else {
             tail.next = newNode;
             newNode.prev = tail;
             tail = newNode;
@@ -23,9 +23,26 @@ public class DoublyLinkedList {
         length++;
     }
 
-    public void printList(){
+    public Node removeLast() {
+        if (this.getLength() == 0) {
+            return null;
+        }
+        Node temp = tail;
+        if (this.length == 1) {
+            head = null;
+            tail = null;
+        } else {
+            tail = temp.prev;
+            tail.next = null;
+            temp.prev = null;
+        }
+        length--;
+        return temp;
+    }
+
+    public void printList() {
         Node temp = head;
-        while (temp != null){
+        while (temp != null) {
             System.out.println(temp.value);
             temp = temp.next;
         }
