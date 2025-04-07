df333aa1030c4993f6312b3b0d6321680a6ecece
rodrilucas <lucas.celery@gmail.com>
2025-04-07 12:47:21 -0300

    doubly linked list initial

---
+++ DoublyLinkedList/src/DoublyLinkedList.java
@@ -0,0 +1,33 @@
+public class DoublyLinkedList {
+    private Node head;
+    private Node tail;
+    private int length;
+
+    public DoublyLinkedList(int value){
+        Node newNode = new Node(value);
+        head = newNode;
+        tail = newNode;
+        length = 1;
+    }
+
+    public void printList(){
+        Node temp = head;
+        while (temp != null){
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+    }
+
+    public Node getHead() {
+        return head;
+    }
+
+    public Node getTail() {
+        return tail;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+}
---
+++ DoublyLinkedList/src/Node.java
@@ -0,0 +1,9 @@
+public class Node {
+    int value;
+    Node prev;
+    Node next;
+
+    public Node(int value){
+        this.value = value;
+    }
+}
