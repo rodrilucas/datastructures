df8c27d90368b35c63d30669437bfe967e2acfaf
rodrilucas <lucas.celery@gmail.com>
2025-04-10 15:23:25 -0300

    ska array list

---
+++ Stack/src/StackArrayList.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+
+public class StackArrayList<T> {
+
+    private ArrayList<T> stackList = new ArrayList<>();
+
+    public ArrayList<T> getStackList() {
+        return stackList;
+    }
+
+    public void printStack() {
+        for (int i = stackList.size()-1; i >= 0; i--) {
+            System.out.println(stackList.get(i));
+        }
+    }
+
+    public boolean isEmpty() {
+        return stackList.isEmpty();
+    }
+
+    public T peek() {
+        if (isEmpty()) {
+            return null;
+        } else {
+            return stackList.get(stackList.size() - 1);
+        }
+    }
+
+    public int size() {
+        return stackList.size();
+    }
+
+    public void push(T value) {
+        stackList.add(value);
+    }
+
+}
