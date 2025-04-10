9229aa89eb87db72454aea102862e7f74067a0fd
rodrilucas <lucas.celery@gmail.com>
2025-04-10 16:25:34 -0300

    ska reverse string

--- Stack/src/StackArrayList.java
+++ Stack/src/StackArrayList.java
@@ -1,4 +1,6 @@
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class StackArrayList<T> {
 
@@ -13,6 +15,18 @@ public class StackArrayList<T> {
             return null;
         }
         return stackList.remove(stackList.size() - 1);
+    }
+
+    public String reverseString(String word) {
+        StackArrayList<Object> stack = new StackArrayList<>();
+        String reverseString = "";
+        for (int i = 0; i < word.length(); i++) {
+            stack.push(word.charAt(i));
+        }
+        while (!stack.isEmpty()){
+            reverseString += stack.pop();
+        }
+        return reverseString;
     }
 
     public void printStack() {
