89c686628b16a033e6ad5f85785756db4a7b5c24
rodrilucas <lucas.celery@gmail.com>
2025-04-10 14:32:18 -0300

    qe initial

---
+++ Queue/src/Queue.java
@@ -0,0 +1,32 @@
+public class Queue {
+    private Node first;
+    private Node last;
+    private int length;
+
+    public Queue(int value) {
+        Node newNode = new Node(value);
+        first = newNode;
+        last = newNode;
+        length = 1;
+    }
+
+    public void printQueue() {
+        Node temp = first;
+        while (temp != null) {
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+    }
+
+    public Node getFirst() {
+        return first;
+    }
+
+    public Node getLast() {
+        return last;
+    }
+
+    public int getLength() {
+        return length;
+    }
+}
